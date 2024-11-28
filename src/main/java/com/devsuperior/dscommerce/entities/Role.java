be3699710b8a5c4b7e33c77512ad138d0b3285ee
marcelocruzsilverio <marcelosilveriocruz@gmail.com>
2024-11-28 15:20:23 -0300

    Implemented spring security Part-1, UserDetails and GrantedAuthority

--- src/main/java/com/devsuperior/dscommerce/entities/Role.java
+++ src/main/java/com/devsuperior/dscommerce/entities/Role.java
@@ -6,10 +6,11 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import org.springframework.security.core.GrantedAuthority;
 
 @Entity
 @Table(name = "tb_role")
-public class Role {
+public class Role implements GrantedAuthority {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -28,6 +29,8 @@ public class Role {
     public void setId(Long id) {
         this.id = id;
     }
+
+    @Override
     public String getAuthority() {
         return authority;
     }
